2cfd36bcf4492a7510eedbab7025205ed8fd63e3
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-02-04 02:30:28 -0500

    Update boot.php

--- boot.php
+++ boot.php
@@ -3,4 +3,7 @@
  * This file is a part of Nenglish7/Optimize.
  *
  * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>
+ *
+ * @link    <https://github.com/Nenglish7/Optimize> Github Repository.
+ * @license <https://github.com/Nenglish7/Optimize/blob/master/LICENSE> Apache-2.0
  */
